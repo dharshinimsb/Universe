b7428bd31ae089af9d83582c8df982b05e7f97d3
dharshinimsb <dharshinimsb@gmail.com>
2025-04-06 16:40:37 +0530

    1st Branch changes

--- src/love/Magic.java
+++ src/love/Magic.java
@@ -4,7 +4,10 @@ public class Magic {
 
 	public static void main(String[] args) {
 		System.out.println("I Love You VinabhavKumaran..!!!");
-
 	}
 
+	public void MagicLand()
+	{
+		System.out.println("God Bless You Both of You..!!!");
+	}
 }
